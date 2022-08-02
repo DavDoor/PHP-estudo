83c7476b77dd467a9e6e140c0b8c95fd88ec0c4a
Davdoor <davidmusic53@gmail.com>
2022-08-02 16:39:55 -0300

    git comanados

--- query_parameters.php
+++ query_parameters.php
@@ -1,5 +1,5 @@
 <?php
-
+    //URL "http://localhost/estudo/query_parameters.php?name=maria&idade=25&altura=1.70&peso=70" para teste.
     var_dump($_GET);
     $name = $_GET["name"];
     $idade = $_GET["idade"];
@@ -7,5 +7,6 @@
     $peso = $_GET["peso"];
 
     echo $name . " " . $idade . " " . $altura . " " . $peso;
+
     
 ?>
