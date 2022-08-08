390a57e48ecd7e4ee62ec825d83f3219fd82f1ea
Davdoor <davidmusic53@gmail.com>
2022-08-08 09:38:49 -0300

    calculadora array 2

--- calculadora _array.php
+++ calculadora _array.php
@@ -1,14 +1,40 @@
 <?php
     //printar uma tabuada com contador
 
-    $tabuada = array("1","2","3","4","5","6","7","8","9", "10");
+    $tabuada = array("1","2","3","4","5","6","7","8","9","10");
 
     $i = 10; //quando mudo a variavel, me retorna o valor diferente multiplicado por ela
 
     for($n=0; $n < count($tabuada); $n++){
 
-        echo  $tabuada[$n]*$i;
+        $somaTotal =  $tabuada[$n]*$i;
+        echo  $tabuada[$n] . " x " . $i . " = " . $somaTotal;
         echo "<br><br>";
     }
 
+    echo "<br>";
+
+    $i = 2; //quando mudo a variavel, me retorna o valor diferente multiplicado por ela
+
+    for($n=1; $n <= 10; $n++){
+
+        $somaTotal =  $n * $i;
+        echo  $n . " x " . $i . " = " . $somaTotal;
+        echo "<br><br>";
+    }
+
+    echo "<br>";
+
+    for ($i=1; $i <= 10; $i++){
+        echo "Printando tabuada de " . $i;
+        echo "<br>"; 
+
+        for($n=1; $n <= 10; $n++){
+
+            $somaTotal =  $n * $i;
+            echo  $n . " x " . $i . " = " . $somaTotal;
+            echo "<br><br>";
+        }  
+        echo "<br>"; 
+    } 
 ?>
---
+++ exerc-par-impar.php
@@ -0,0 +1,6 @@
+<?php
+    //par ou impar
+    //usar o for de 10 até 100 por exemplo
+    //verfiicar o for, colocando o if ($i % 2 == 0) será par
+
+?>
