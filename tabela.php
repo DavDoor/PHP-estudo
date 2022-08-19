b4263abbf3f642e687ab0f7dd1d4388af799c074
Davdoor <davidmusic53@gmail.com>
2022-08-18 22:56:25 -0300

    criacção de tabelas

--- tabela.php
+++ tabela.php
@@ -1,4 +1,38 @@
-<?php
-    //criar uma tabela com com clientes, compras, etc... 
+<!--
+    //criar uma tabela com clientes, compras, etc... 
     //table
-?>
+-->
+<style>
+    table, tr, td, th {
+        border: 0.1px solid black
+    }
+</style>
+
+<table style="width:50%">
+    <tr>
+        <th>Clientes</th>
+        <th>Produto</th>
+        <th>Valor da compra</th>
+    </tr>
+    <tr>
+        <td>David</td>
+        <td>Bermuda</td>
+        <td>R$ 60,00</td>
+    <tr>
+        <td>Michelly</td>
+        <td>Secador</td>
+        <td>R$ 100,00</td>
+    </tr>
+    <tr>
+        <td>Rafael</td>
+        <td>Camisa</td>
+        <td>R$ 50,00</td>
+    </tr>
+    <tr>
+        <td>Camila</td>
+        <td>Vestido</td>
+        <td>R$ 80,00</td>
+    </tr>
+</table>
+
+
