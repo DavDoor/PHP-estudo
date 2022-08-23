6b19f49bd671691bcc7424f22f1f36e91ca1fbe5
Davdoor <davidmusic53@gmail.com>
2022-08-23 18:27:39 -0300

    switch

---
+++ switch_1.php
@@ -0,0 +1,25 @@
+<?php
+    //Comparação entre a função "IF" e "SWITCH"
+
+if ($i == 0) {
+    echo "i é igual a 0";
+} elseif ($i == 1) {
+    echo "i é igual a 1";
+} elseif ($i == 2) {
+    echo "i é igual a 2";
+}
+
+
+
+switch ($i) {
+    case 0:
+        echo "i é igual a 0";
+        break;
+    case 1:
+        echo "i é igual a 1";
+        break;
+    case 2:
+        echo "i é igual a 2";
+        break;
+}
+?>
---
+++ switch_2_sorteio.php
@@ -0,0 +1,18 @@
+<?php
+    $sorteio = rand(0,5); //número aleatório sorteado
+
+    switch($sorteio){
+       case 0:
+             echo "Você perdeu a vez";
+             break;
+       case 1:
+             echo "Avance 1 casa";
+             break;
+       case 2:
+             echo "Você ganhou um bônus! Avance 3 casas";
+             break;
+       default:
+             echo "Jogue novamente";
+             break;
+    }
+?>
